b70af9e47a74fcefaf611da740c3f41ad15e7c93
Felix <felixrigau88@gmail.com>
2017-11-17 15:49:32 +0100

    Adding the methods createCliente and removeClient

--- class14-objects/application.js
+++ class14-objects/application.js
@@ -3,22 +3,46 @@
 })();
 
 var APPLICATION = APPLICATION || {
-  users: {},
-  product: {},
-  VendingMachine: {
-    
-  }
+  clientList: [],
+  productList: [],
+
+  createClient: function (name, userName, password, userType, budget) {
+    var client = new Client(name, userName, password, userType, budget);
+    this.clientList.push(client);
+    console.log("The client: "+name+" was registed successfuly!");
+  },
+
+  removeClient: function (name){
+    var position = -1;
+    this.clientList.forEach(function (element,i,array) {
+      if(element.name == name){
+        position = i;
+      }
+    });
+    if(position !== -1){
+      this.clientList.splice(position,1);
+      console.log("The client: "+name+" was removed successfuly!");
+    }else {
+      console.log(name+" isn't a client in this Vending Machine!");
+    }
+  },
 
 };
 
-var Client = function Client(name, userName, password, userType, budget){
+function Client(name, userName, password, userType, budget){
   this.name = name;
   this.userName = userName;
   this.password = password;
   this.userType = userType;
   this.budget = budget;
+  this.logged = false;
 }
 
 Client.prototype.sayHello = function () {
   console.log("Hello, my name's " + this.name);
 };
+
+function Product(name, price){
+  this.name = name;
+  this.price = price;
+}
