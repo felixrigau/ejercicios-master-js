089642d53953eda1e832d7ddd4d5b4ad479044ea
Felix <felixrigau88@gmail.com>
2017-11-17 16:34:37 +0100

    Adding verification permissions administrator funcionality for all methods of the client

--- class14-objects/application.js
+++ class14-objects/application.js
@@ -7,62 +7,80 @@ var APPLICATION = APPLICATION || {
   productList: [],
 
   createClient: function (name, userName, password, userType, budget) {
-    var exist = this.clientList.some(function (element, index, array) {
-      return element.userName == userName
-    });
-    if (exist) {
-      console.log(userName+" is already registed.");
-    } else {
-      if (name && userName && password && userType && budget) {
-        var client = new Client(name, userName, password, userType, budget);
-        this.clientList.push(client);
-        console.log("The client: "+name+" was registed successfuly!");
+    var passwordAdmin = prompt("Please, enter your password to perform the next opetation:");
+    if (passwordAdmin == "ficticiaMola") { // Verify that the user has admin permissions
+      var exist = this.clientList.some(function (element, index, array) {
+        return element.userName == userName
+      });
+      if (exist) {
+        console.log(userName+" is already registed.");
       } else {
-        console.log("Upsss! You haven't registed all the necesary information!");
+        if (name && userName && password && userType && budget) {
+          var client = new Client(name, userName, password, userType, budget);
+          this.clientList.push(client);
+          console.log("The client: "+name+" was registed successfuly!");
+        } else {
+          console.log("Upsss! You haven't registed all the necesary information!");
+        }
       }
+    }else{
+      console.log("You haven't permissions for this operation.");
     }
+
   },
 
   removeClient: function (userName){
-    var position = -1;
-    this.clientList.forEach(function (element,i,array) {
-      if(element.userName == userName){
-        position = i;
+    if (passwordAdmin == "ficticiaMola") { // Verify that the user has admin permissions
+      var position = -1;
+      this.clientList.forEach(function (element,i,array) {
+        if(element.userName == userName){
+          position = i;
+        }
+      });
+      if(position !== -1){
+        this.clientList.splice(position,1);
+        console.log("The client: "+userName+" was removed successfuly!");
+      }else {
+        console.log(userName+" isn't a client in this Vending Machine!");
       }
-    });
-    if(position !== -1){
-      this.clientList.splice(position,1);
-      console.log("The client: "+userName+" was removed successfuly!");
-    }else {
-      console.log(userName+" isn't a client in this Vending Machine!");
+    }else{
+      console.log("You haven't permissions for this operation.");
     }
   },
 
   getClientBudget: function (userName) {
-    var budget = -1;
-    this.clientList.forEach(function (element,i,array) {
-      if(element.userName == userName){
-        budget = element.budget;
+    if (passwordAdmin == "ficticiaMola") { // Verify that the user has admin permissions
+      var budget = -1;
+      this.clientList.forEach(function (element,i,array) {
+        if(element.userName == userName){
+          budget = element.budget;
+        }
+      });
+      if(budget !== -1){
+        return budget;
+      }else {
+        console.log(" We cannot to get the budget for "+userName);
       }
-    });
-    if(budget !== -1){
-      return budget;
-    }else {
-      console.log(" We cannot to get the budget for "+userName);
+    }else{
+      console.log("You haven't permissions for this operation.");
     }
   },
 
   getClientExpenses: function (userName) {
-    var expensesList = false;
-    this.clientList.forEach(function (element,i,array) {
-      if(element.userName == userName){
-        expensesList = element.expenses;
+    if (passwordAdmin == "ficticiaMola") { // Verify that the user has admin permissions
+      var expensesList = false;
+      this.clientList.forEach(function (element,i,array) {
+        if(element.userName == userName){
+          expensesList = element.expenses;
+        }
+      });
+      if (expensesList) { //if expensesList is full, get in. Else expensesList is false because userName was not found
+        return expensesList;
+      } else {
+        return expensesList;
       }
-    });
-    if (expensesList) { //if expensesList is full, get in. Else expensesList is false because userName was not found
-      return expensesList;
-    } else {
-      return expensesList;
+    }else{
+      console.log("You haven't permissions for this operation.");
     }
   }
 
@@ -91,3 +109,9 @@ function Expense(quantity, date){
   this.name = name;
   this.price = price;
 }
+
+// if (passwordAdmin == "ficticiaMola") { // Verify that the user has admin permissions
+//
+// }else{
+//   console.log("You haven't permissions for this operation.");
+// }
