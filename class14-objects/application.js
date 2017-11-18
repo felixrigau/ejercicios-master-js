7d9c087895baacc601a88f2078a41d660b7f0cdf
Felix <felixrigau88@gmail.com>
2017-11-18 17:11:07 +0100

    Added something methods for the management of the products

--- class14-objects/application.js
+++ class14-objects/application.js
@@ -6,11 +6,12 @@ var APPLICATION = APPLICATION || {
   clientList: [],
   productList: [],
 
+  //Client Management
   createClient: function (name, userName, password, userType, budget) {
-    var passwordAdmin = prompt("Please, enter your password to perform the next opetation:");
+    var passwordAdmin = prompt("Please, enter your password to perform the next operation:");
     if (passwordAdmin == "ficticiaMola") { // Verify that the user has admin permissions
       var exist = this.clientList.some(function (element, index, array) {
-        return element.userName == userName
+        return element.userName == userName;
       });
       if (exist) {
         console.log(userName+" is already registed.");
@@ -19,8 +20,9 @@ var APPLICATION = APPLICATION || {
           var client = new Client(name, userName, password, userType, budget);
           this.clientList.push(client);
           console.log("The client: "+name+" was registed successfuly!");
+          return client;
         } else {
-          console.log("Upsss! You haven't registed all the necesary information!");
+          console.log("Upsss! You haven't registed all the necesary information to resgister a new client!");
         }
       }
     }else{
@@ -30,6 +32,7 @@ var APPLICATION = APPLICATION || {
   },
 
   removeClient: function (userName){
+    var passwordAdmin = prompt("Please, enter your password to perform the next operation:");
     if (passwordAdmin == "ficticiaMola") { // Verify that the user has admin permissions
       var position = -1;
       this.clientList.forEach(function (element,i,array) {
@@ -48,7 +51,39 @@ var APPLICATION = APPLICATION || {
     }
   },
 
+  getClient:function (userName) {
+    var passwordAdmin = prompt("Please, enter your password to perform the next operation:");
+    if (passwordAdmin == "ficticiaMola") { // Verify that the user has admin permission
+      var client = this.clientList.filter(function (element,i,array) {
+        return element.userName === userName;
+      });
+      if(client.length != 0){
+        return client[0];
+      }else {
+        console.log(userName+" don't exist");
+        return null;
+      }
+    }else{
+      console.log("You haven't permissions for this operation.");
+    }
+  },
+
+  verifyCredentialsClient:function (userName, password) {
+    var client = this.getClient(userName);
+    if (client) {
+      if (client.password === "password") {
+        return client;
+      } else {
+        return false;
+      }
+    } else {
+      console.log(userName+" don't exist");
+      return false;
+    }
+  },
+
   getClientBudget: function (userName) {
+    var passwordAdmin = prompt("Please, enter your password to perform the next operation:");
     if (passwordAdmin == "ficticiaMola") { // Verify that the user has admin permissions
       var budget = -1;
       this.clientList.forEach(function (element,i,array) {
@@ -67,6 +102,7 @@ var APPLICATION = APPLICATION || {
   },
 
   getClientExpenses: function (userName) {
+    var passwordAdmin = prompt("Please, enter your password to perform the next operation:");
     if (passwordAdmin == "ficticiaMola") { // Verify that the user has admin permissions
       var expensesList = false;
       this.clientList.forEach(function (element,i,array) {
@@ -82,7 +118,110 @@ var APPLICATION = APPLICATION || {
     }else{
       console.log("You haven't permissions for this operation.");
     }
-  }
+  },
+
+  //Product Management
+  createProduct: function (name, price) {
+    var passwordAdmin = prompt("Please, enter your password to perform the next operation:");
+    if (passwordAdmin == "ficticiaMola") { // Verify that the user has admin permissions
+      var exist = this.productList.some(function (element, index, array) {
+        return element.name == name;
+      });
+      if (exist) {
+        console.log("This product is already registed.");
+      } else {
+        if (name && price) {
+          var product = new Product(name, price);
+          this.productList.push(product);
+          console.log("The product: "+name+" was registed successfuly!");
+        } else {
+          console.log("Upsss! You haven't registed all the necesary information to register a new product!");
+        }
+      }
+    }else{
+      console.log("You haven't permissions for this operation.");
+    }
+  },
+
+  removeProduct: function (productName) {
+    var passwordAdmin = prompt("Please, enter your password to perform the next operation:");
+    if (passwordAdmin == "ficticiaMola") { // Verify that the user has admin permissions
+      var position = -1;
+      this.productList.forEach(function (element,i,array) {
+        if(element.name == productName){
+          position = i;
+        }
+      });
+      if(position !== -1){
+        this.productList.splice(position,1);
+        console.log("The product: "+productName+" was removed successfuly!");
+      }else {
+        console.log(productName+" don't exist in Vending Machine Stock!");
+      }
+    }else{
+      console.log("You haven't permissions for this operation.");
+    }
+  },
+
+  addUnitsProduct: function (productName, quantity) {
+    var passwordAdmin = prompt("Please, enter your password to perform the next operation:");
+    if (passwordAdmin == "ficticiaMola") { // Verify that the user has admin permission
+      var product = this.productList.filter(function (element,i,array) {
+        return element.name === productName;
+      });
+      if(product.length != 0){
+        product[0].quantity += quantity;
+        console.log("The product: "+productName+" has already "+product[0].quantity+" units in stock.");
+      }else {
+        console.log(productName+" don't exist in Vending Machine Stock!");
+      }
+    }else{
+      console.log("You haven't permissions for this operation.");
+    }
+  },
+
+  verifyUnitsOfProduct:function (productName) {
+    var product = this.productList.filter(function (element,i,array) {
+      return element.name === productName;
+    });
+    if (product.length != 0) {
+      if (product[0].quantity != 0) {
+        return product;
+      } else {
+        console.log("Sorry!, this product is sold out!");
+        return false;
+      }
+    } else {
+      console.log(productName+" don't exist at Stock!.");
+      return false;
+    }
+  },
+
+  consumeAProduct: function (productName, userName, password) {
+    var client = this.verifyCredentialsClient(userName, password);
+    if (client) {
+      var product = verifyUnitsOfProduct(productName);
+      if (product) {
+        if (client.budget >= product.price) {
+          //TODO
+          //To add an expense to client's expenses list
+          var expense = Expense(product.price, product.name, new Date());
+          client.expenses.push(expense);
+          //To remove the sum of the client's getClientBudget
+          client.budget -= product.price;
+          //To remove an unit of the product
+          product.quantity--;
+        } else {
+          console.log("Sorry, your budget isn't enough. You must recharge it.");
+        }
+      } else {
+        console.log("The product don't exist or not exist units of the product into Stock!");
+        return -1;
+      }
+    } else {
+      console.log("The client don't exist or haven`t the permissions to consume this product.");
+    }
+  },
 
 };
 
@@ -103,13 +242,18 @@ Client.prototype.sayHello = function () {
 function Product(name, price){
   this.name = name;
   this.price = price;
+  this.quantity = 0;
 }
 
-function Expense(quantity, date){
+function Expense(quantity, productName, date){
+  this.quantity = quantity;
+  this.productName = productName;
   this.name = name;
-  this.price = price;
 }
 
+// Falta hacer el paso 5 y el 6
+
+// var passwordAdmin = prompt("Please, enter your password to perform the next operation:");
 // if (passwordAdmin == "ficticiaMola") { // Verify that the user has admin permissions
 //
 // }else{
