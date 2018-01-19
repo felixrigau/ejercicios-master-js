9a2f2e25a84ee2a7b641e998cad6d4165ef3d2f2
Felix Rigau <felixrigau88@gmail.com>
2018-01-19 15:44:11 +0000

    adding firebase.auth() to authenticate the user - ERROR

--- class42-FirebaseLoginExercise/js/scripts.js
+++ class42-FirebaseLoginExercise/js/scripts.js
@@ -1,10 +1,22 @@
 // Empty JS for your own code to be here
+/*global firebase*/
 function addBehaviorToLoginBUtton() {
-  var loginButton = document.querySelector('.register');
-  loginButton.addEventListener('click',)
+    var loginButton = document.querySelector('.register');
+    loginButton.addEventListener('click',loginUser)
 }
 
 function loginUser(event) {
-  var email = document.querySelector('.email').value;
-  var password = document.querySelector('.password').value;
+    var email = document.querySelector('.email').value;
+    var password = document.querySelector('.password').value;
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here
+        console.log(error)
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+    });
 }
+
+(function () {
+    addBehaviorToLoginBUtton()
+})();
